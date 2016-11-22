5c1fcda5cd8dcda61ab91786f7e0f005eb471e2d
Simon H <huening.simon@googlemail.com>
2016-11-22 20:01:40 +0100

    implemented step 1 and 2

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ passwd = sys.argv[2]
 db = sys.argv[3]
 
 # config
-wordIdBoundary = 1000
+wordIdBoundary = 10000
 minCountOfWords = 5
 
 # create db connection
@@ -19,46 +19,32 @@ db = MySQLdb.connect(host = host,
                     passwd = passwd,
                     db = db)
 
-# get word id from db
-# if word does not exist or its word id is lower than wordIdBoundary, then return None
-def getWordId(word):
-    cursor = db.cursor()
-    query = "select w_id from words where w_id > " + str(wordIdBoundary) + " and word = '" + word + "'"
-    cursor.execute(query)
-    result = cursor.fetchone()
-    cursor.close()
-    if result != None:
-        return result[0]
-    else:
-        return None
-
-def substituteWords(sentence):
-    words = sentence.split(" ")
-    wordVector = []
-    for word in words:
-        wordId = getWordId(word)
-        if wordId != None:
-            wordVector.append(wordId)
-    return wordVector
-
 def createWordVector(col):
     from collections import defaultdict
-    result = defaultdict(list)
-    collen = len(col)
-    i = 0
+    result = defaultdict(set)
     for row in col:
-        progress = i / collen * 100
-        if i % 10 == 5:
-            print("Progress-Map: " + str(progress))
-        result[row[0]].append(row[1])
-        i += 1
+        result[row[0]].add(row[1])
     return result
 
+def createSentenceVector(wordVectors):
+    from collections import defaultdict
+    result = defaultdict(set)
+    for sentenceId in wordVectors.keys():
+        for wordId in wordVectors[sentenceId]:
+            result[wordId].add(sentenceId)
+    #remove all sentence vectors with less than 2 entries
+    for wordId in result.keys():
+        if len(result[wordId]) == 1:
+            result.pop(wordId)
+    return result
+
+
 t1 = clock()
-query = "select i1.s_id, i1.w_id from inv_w as i1 join (select s_id from inv_w where w_id >= " + str(wordIdBoundary) + " group by s_id having count(*) >= " + str(minCountOfWords) + ") as i2 on i1.s_id = i2.s_id where i1.w_id >= " + str(wordIdBoundary) + " order by i1.s_id"
+query = "select i1.s_id, i1.w_id from inv_w as i1 join (select s_id from inv_w where w_id >= " + str(wordIdBoundary) + " group by s_id having count(*) >= " + str(minCountOfWords) + " limit 50) as i2 on i1.s_id = i2.s_id where i1.w_id >= " + str(wordIdBoundary) + " order by i1.s_id"
 c1 = db.cursor()
 c1.execute(query)
-len(createWordVector(c1.fetchall()))
+wordVectors = createWordVector(c1.fetchall())
+sentenceVectors = createSentenceVector(wordVectors)
 c1.close()
 t2 = clock()
 print(str(t2 - t1))
