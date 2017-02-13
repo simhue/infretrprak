2d34a422276829d60ad9b04bb0c69367c5824d37
Simon Hüning <ir2016-gruppe8@praksrv.informatik.uni-leipzig.de>
2017-02-13 15:25:43 +0100

    removed 'unique flag' of sort command

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 date > begin
 python main.py
-sort --parallel=3 -uo sorted-pairs pairs
-rm pairs
+sort --parallel=3 -o sorted-pairs pairs
+#rm pairs
 cat sorted-pairs | uniq -d -c > counted-pairs
-rm sorted-pairs
-sort --parallel=3 -uo sorted-counted-pairs counted-pairs
-rm counted-pairs
+#rm sorted-pairs
+sort --parallel=3 -o sorted-counted-pairs counted-pairs
+#rm counted-pairs
 date > end
